ada5dfb268a337aaee650ce0ea656aafb74d0169
zz <zil10@inbox.ru>
2025-03-10 19:15:31 +0700

    api

--- api/other_api_parts/login_api.py
+++ api/other_api_parts/login_api.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, request, jsonify
 
 import data.db_session as db_session
+from api.check_api import check_api
 from data.user import User
 
 blueprint = Blueprint('login_api', __name__, template_folder='templates')
 
 
 @blueprint.route('/api/check_authorization_data')
-def create_news():
+@check_api
+def check_authorization_data():
     if not request.json:
         return jsonify({'error': 'Empty request'})
     if not all(key in request.json for key in ['email', 'password']):
@@ -15,5 +17,5 @@ def create_news():
     session = db_session.create_session()
     user = session.query(User).filter(User.email == request.json['email']).first()
     if user and user.check_password(request.json['password']):
-        return jsonify({'result': True})
-    return jsonify({'result': False})
+        return jsonify({'user_id': user.id, 'result': True})
+    return jsonify({'user_id': user.id, 'result': False})
